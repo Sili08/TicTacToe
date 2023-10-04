3ea1d74395d39389adc57a87b80d12bca0cea916
Silas Bruellhardt <silas.bruellhardt@abf.ch>
2023-10-04 08:37:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function Click(Player1, Player2) {
+    if (Player1 === pressed()) {
+        return x;
+    }
+    else if (Player2 === pressed) {
+        return o;
+    }
+    else {
+        return;
+    }
+}
+
+function pressed() {
+    
+}
